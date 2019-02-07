a0bc6d58fc3309d8fd2e744e420d1ed23ee0253a
Allan Wang <me@allanwang.ca>
2019-02-07 01:10:20 -0500

    Add missing console log

--- gh-pages.sh
+++ gh-pages.sh
@@ -6,7 +6,7 @@ mv ./.gitignore ./.gitignore-bak
 mv ./.gitignore-gh-pages ./.gitignore
 
 rm ./js/Github.js
-echo "--------------------------------------------------------------------------------" >> ./js/Github.js
+echo "console.log('--------------------------------------------------------------------------------');" >> ./js/Github.js
 echo "console.log('Launched with commit $TRAVIS_COMMIT');" >> ./js/Github.js
 echo "console.log('View the source at https://github.com/patacat/patacat/tree/$TRAVIS_COMMIT');" >> ./js/Github.js
-echo "--------------------------------------------------------------------------------" >> ./js/Github.js
+echo "console.log('--------------------------------------------------------------------------------');" >> ./js/Github.js
