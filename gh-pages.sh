b6e554bde0fd1554fcb27b863bce7996e8dfeb7b
Allan Wang <me@allanwang.ca>
2019-02-07 00:59:21 -0500

    Add extra log

--- gh-pages.sh
+++ gh-pages.sh
@@ -7,3 +7,4 @@ mv ./.gitignore-gh-pages ./.gitignore
 
 rm ./js/Github.js
 echo "console.log('Launched with commit $TRAVIS_COMMIT');\n" >> ./js/Github.js
+echo "console.log('View the source at https://github.com/patacat/patacat/tree/$TRAVIS_COMMIT');\n" >> ./js/Github.js
